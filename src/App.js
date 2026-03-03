9a420258064097f8602a34c7abc5677b78af8f48
Monserrath Valenzuela <monserrathvalenz@outlook.com>
2026-03-02 18:54:08 -0600

    Lab 4 - CRUD simple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,175 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Table, Button, Container, FormGroup,
+  Modal, ModalHeader, ModalBody, ModalFooter,
+} from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { id: 1, nombre: "Jorge Carranza", empresa: "Tec" },
+  { id: 2, nombre: "Ramon Velez", empresa: "Banorte" },
+  { id: 3, nombre: "Hugo Sanchez", empresa: "Real Madrid" },
+  { id: 4, nombre: "Rafael Marquez", empresa: "Barcelona" },
+  { id: 5, nombre: "Carlos Alcaraz", empresa: "Mallorca" },
+  { id: 6, nombre: "N. Djokovic", empresa: "Serbia" },
+  { id: 7, nombre: "Sergio Perez", empresa: "Cadillac" },
+  { id: 8, nombre: "Max Verstapen", empresa: "Oracle Red Bull Racing" },
+  { id: 9, nombre: "Carlos Sainz", empresa: "Williams Racing" },
+];
+
+class App extends React.Component {
+  state = {
+    data: data,
+    modalActualizar: false,
+    modalInsertar: false,
+    form: { id: "", nombre: "", empresa: "" },
+  };
+
+  mostrarModalActualizar = (dato) => {
+    this.setState({ form: dato, modalActualizar: true });
+  };
+
+  cerrarModalActualizar = () => {
+    this.setState({ modalActualizar: false });
+  };
+
+  mostrarModalInsertar = () => {
+    this.setState({ modalInsertar: true });
+  };
+
+  cerrarModalInsertar = () => {
+    this.setState({ modalInsertar: false });
+  };
+
+  editar = (dato) => {
+    var contador = 0;
+    var arreglo = this.state.data;
+    arreglo.map((registro) => {
+      if (dato.id === registro.id) {
+        arreglo[contador].nombre = dato.nombre;
+        arreglo[contador].empresa = dato.empresa;
+      }
+      contador++;
+      return registro;
+    });
+    this.setState({ data: arreglo, modalActualizar: false });
+  };
+
+  eliminar = (dato) => {
+    var opcion = window.confirm("¿Estás seguro que deseas eliminar el elemento " + dato.id + "?");
+    if (opcion === true) {
+      var contador = 0;
+      var arreglo = this.state.data;
+      arreglo.map((registro) => {
+        if (dato.id === registro.id) {
+          arreglo.splice(contador, 1);
+        }
+        contador++;
+        return registro;
+      });
+      this.setState({ data: arreglo, modalActualizar: false });
+    }
+  };
+
+  insertar = () => {
+    var valorNuevo = { ...this.state.form };
+    valorNuevo.id = this.state.data.length + 1;
+    var lista = this.state.data;
+    lista.push(valorNuevo);
+    this.setState({ modalInsertar: false, data: lista });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Container>
+          <br />
+          <Button color="success" onClick={() => this.mostrarModalInsertar()}>Crear</Button>
+          <br /><br />
+          <Table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Empresa</th>
+                <th>Acción</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map((dato) => (
+                <tr key={dato.id}>
+                  <td>{dato.id}</td>
+                  <td>{dato.nombre}</td>
+                  <td>{dato.empresa}</td>
+                  <td>
+                    <Button color="primary" onClick={() => this.mostrarModalActualizar(dato)}>Editar</Button>{" "}
+                    <Button color="danger" onClick={() => this.eliminar(dato)}>Eliminar</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+
+        {/* Modal Insertar */}
+        <Modal isOpen={this.state.modalInsertar}>
+          <ModalHeader><h3>Insertar nombre</h3></ModalHeader>
+          <ModalBody>
+            <FormGroup>
+              <label>Id: </label>
+              <input className="form-control" readOnly type="text" value={this.state.data.length + 1} />
+            </FormGroup>
+            <FormGroup>
+              <label>Nombre: </label>
+              <input className="form-control" name="nombre" type="text" onChange={this.handleChange} />
+            </FormGroup>
+            <FormGroup>
+              <label>Empresa: </label>
+              <input className="form-control" name="empresa" type="text" onChange={this.handleChange} />
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={() => this.insertar()}>Insertar</Button>
+            <Button color="danger" onClick={() => this.cerrarModalInsertar()}>Cancelar</Button>
+          </ModalFooter>
+        </Modal>
+
+        {/* Modal Editar */}
+        <Modal isOpen={this.state.modalActualizar}>
+          <ModalHeader><h3>Editar Registro</h3></ModalHeader>
+          <ModalBody>
+            <FormGroup>
+              <label>Id: </label>
+              <input className="form-control" readOnly type="text" value={this.state.form.id} />
+            </FormGroup>
+            <FormGroup>
+              <label>Nombre: </label>
+              <input className="form-control" name="nombre" type="text"
+                onChange={this.handleChange} value={this.state.form.nombre} />
+            </FormGroup>
+            <FormGroup>
+              <label>Empresa: </label>
+              <input className="form-control" name="empresa" type="text"
+                onChange={this.handleChange} value={this.state.form.empresa} />
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={() => this.editar(this.state.form)}>Editar</Button>
+            <Button color="danger" onClick={() => this.cerrarModalActualizar()}>Cancelar</Button>
+          </ModalFooter>
+        </Modal>
+      </>
+    );
+  }
 }
 
 export default App;
